1016fc384ccb58deeb4e7871f0f76142ed66bae8
Emmanuel Dufe <142026793+Kwameeh@users.noreply.github.com>
2025-10-25 13:22:04 +0000

    Fix TypeScript error in services page - add proper type annotation for expertise parameter

--- app/(website)/services/page.tsx
+++ app/(website)/services/page.tsx
@@ -1,6 +1,9 @@
 export const revalidate = 0;
 
-import { GALLERY_BY_SLUG_QUERYResult } from "@/sanity.types";
+import {
+  GALLERY_BY_SLUG_QUERYResult,
+  ALL_EXPERTISES_QUERYResult,
+} from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
 import { getAllExpertises } from "@/sanity/queries/expertise";
 import { getGalleryBySlug } from "@/sanity/queries/gallery";
@@ -24,25 +27,27 @@ const ServicesPage = async () => {
             <h1>Our Expertise</h1>
           </div>
           <div className="md:grid sm:grid-cols-2 xl:grid-cols-3 gap-3 grid grid-cols-1">
-            {expertises.map((expertise, index: number) => {
-              return (
-                <div
-                  key={index}
-                  className="text-white flex justify-between flex-col gap-3 col-span-2 xl:col-span-1 p-6 border-white border">
-                  <div>
-                    <h2 className="text-xl font-bebas font-bold leading-none">
+            {expertises.map(
+              (expertise: ALL_EXPERTISES_QUERYResult[0], index: number) => {
+                return (
+                  <div
+                    key={index}
+                    className="text-white flex justify-between flex-col gap-3 col-span-2 xl:col-span-1 p-6 border-white border">
+                    <div>
+                      <h2 className="text-xl font-bebas font-bold leading-none">
+                        {expertise.heading}
+                      </h2>
+                    </div>
+                    <div className="text-sm">{expertise.excerpt}</div>
+                    <Link
+                      href={`/services/${expertise.slug?.current || ""}`}
+                      className="text-white border-2 border-white py-3 font-bebas  text-xl text-x text-center hover:bg-white hover:text-black duration-300">
                       {expertise.heading}
-                    </h2>
+                    </Link>
                   </div>
-                  <div className="text-sm">{expertise.excerpt}</div>
-                  <Link
-                    href={`/services/${expertise.slug?.current || ""}`}
-                    className="text-white border-2 border-white py-3 font-bebas  text-xl text-x text-center hover:bg-white hover:text-black duration-300">
-                    {expertise.heading}
-                  </Link>
-                </div>
-              );
-            })}
+                );
+              }
+            )}
 
             <div className="col-span-1 md:col-span-3">
               <Marquee className="h-full w-full" loop={50}>
